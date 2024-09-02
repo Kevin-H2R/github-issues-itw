01cbd065ce30b556235896a86967a8f75ea14ea6
Kevin <herrbach.kevin@gmail.com>
2024-09-02 19:57:19 +0900

    Adds skeletton for all the necessary routes

--- back/routes/issues.js
+++ back/routes/issues.js
@@ -1,8 +1,24 @@
 const express = require('express')
 const router = express.Router()
 
+// Returns all issues
 router.get('/', (req, res) => {
-  res.json('Issue getter')
+  res.json('Gets all issues')
 })
+
+// Returns a single issue by ID
+router.get('/:id', (req, res) => {
+  res.json('Gets issue with id: ' + req.params.id)
+})
+
+// Updates an existing issue
+router.post('/', (req, res) => {
+  res.json('Updates issue with id: ' + req.body.id)
+})
+
+router.put('/', (req, res) => {
+  res.json('Creates issue with id: ' + req.body.id)
+})
+
 
 module.exports = router
