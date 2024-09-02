695fb10db3f489a626a0e3ddf0818662fc34d21c
Kevin <herrbach.kevin@gmail.com>
2024-09-02 19:50:27 +0900

    Creates routes directory and imports route file for issues endpoint

--- back/index.js
+++ back/index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
+const issuesRouter = require('./routes/issues')
+
 
 app.use(express.json())
 
@@ -7,6 +9,8 @@ app.get('/', (req, res) => {
   console.log("Hellow")
   res.json({"message": 'YOW'})
 })
+
+app.use('/issues', issuesRouter)
 app.listen(3000, () => {
   console.log("Hi Sitemate ! App up and running")
 })
