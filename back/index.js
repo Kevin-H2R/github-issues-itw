47523f63148f5d707ef07775d8716b661e637b12
Kevin <herrbach.kevin@gmail.com>
2024-09-02 20:59:21 +0900

    ADds cors middleware on the abck

--- back/index.js
+++ back/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
+var cors = require('cors')
 const issuesRouter = require('./routes/issues')
 
 
+app.use(cors())
 app.use(express.json())
 
 app.get('/', (req, res) => {
