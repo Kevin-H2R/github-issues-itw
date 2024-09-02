d7ef6b584a3fa738640804beb589cca7071a9967
Kevin <herrbach.kevin@gmail.com>
2024-09-02 20:28:38 +0900

    Adds validate put function + factorizes code a bit

--- back/utils/requestValidator.js
+++ back/utils/requestValidator.js
@@ -1,19 +1,10 @@
-exports.validatePost = (req) => {
-  const {id, title, description} = req.body
+const validatePost = (req) => {
+  const {id} = req.body
   if (!id) {
     throw new Error('Missing id attribute')
   }
-  if (!title) {
-    throw new Error('Missing title attribute')
-  }
-  if (!description) {
-    throw new Error('Missing description attribute')
-  }
-  // Example
-  if (title.length > 128) {
-    throw new Error('Title length should be < 128')
-  }
-
+  // I think this lacks clarity, though, for code duplication sake, I used the existing function
+  const {title, description} = validatePut(req)
   const idNumber = Number.parseInt(id)
   if (Number.isNaN(idNumber)) {
     throw new Error("id must be a number");
@@ -21,3 +12,23 @@ exports.validatePost = (req) => {
 
   return {id: idNumber, title, description}
 }
+
+const validatePut = (req) => {
+  const {title, description} = req.body
+  if (!title) {
+    throw new Error('Missing title attribute')
+  }
+  if (!description) {
+    throw new Error('Missing description attribute')
+  }
+
+  // Example
+  if (title.length > 128) {
+    throw new Error('Title length should be < 128')
+  }
+
+  return {title, description}
+}
+
+exports.validatePost = validatePost
+exports.validatePut = validatePut
