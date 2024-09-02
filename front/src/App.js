aa169bad1c2be6ad0d1d25acafd432a028f8f51e
Kevin <herrbach.kevin@gmail.com>
2024-09-02 20:59:35 +0900

    Makes get all endpoint

--- front/src/App.js
+++ front/src/App.js
@@ -1,15 +1,22 @@
 import { useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
 
   const [statusCode, setStatusCode] = useState(null)
-  const [response, setResponse] = useState("")
+  const [issues, setIssues] = useState([])
+
+  const getAll = async () => {
+    const response = await axios.get('http://localhost:4000/issues/')
+    setStatusCode(response.status)
+    setIssues(response.data.data)
+  }
 
   return (
     <div className="main">
       <div className="main__left">
-        <h2>Get ALL</h2>
+        <button onClick={getAll}>Get ALL</button>
         <h2>Get ID:</h2>
         <h2>Create:</h2>
         <h2>Update:</h2>
@@ -21,7 +28,7 @@ function App() {
           {statusCode && <h2>{statusCode}</h2>}
         </div>
         <div>Body:</div>
-        <div>{response}</div>
+        <div>{issues.map(issue => <div key={'issue_' + issue.id}>id: {issue.id}, title: {issue.title}, description: {issue.description}</div>)}</div>
       </div>
     </div>
   );
